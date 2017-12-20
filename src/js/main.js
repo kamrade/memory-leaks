3a7e2a58fc42d65ac4dddb61a225fcf3644abb72
Denis Michailov <kamrad.reg@gmail.com>
2017-12-20 20:26:26 +1000

    memory leaks starts

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,17 @@
-// Libraries
+console.log('--- initial ---');
 
-let $ = require('jquery');
-window.$ = $;
-window.jquery = $;
-window.jQuery = $;
+// FIRST memory leak
 
-// let Tether = require('../../node_modules/tether/dist/js/tether');
-// window.Tether = Tether;
-// require('../../node_modules/bootstrap/dist/js/bootstrap');
-// let Handlebars = require('handlebars');
+var x = [];
+function grow() {
+  for (var i = 0; i < 10000; i++) {
+    document.body.appendChild(document.createElement('div'));
+  }
+  x.push(new Array(1000000).join('x'));
+}
 
-let simpleArray = [1,2,3];
-let newSimpleArray = [ ...simpleArray, 4, 5 ];
-let img = () => ({ ret: 42 });
-let importedModule = require('./module');
+document.getElementById('grow').addEventListener('click', grow);
+
+// SECOND memory leak
+
+var row = document.getElementById('row');
